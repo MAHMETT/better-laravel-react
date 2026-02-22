019f261ec9bd08fc32cf3fa9164d080d91aaaebb
MAHMETT <muhamatjayakusuma2008@gmail.com>
2026-02-22 16:33:23 +0700

    add badge role on user info

--- resources/js/components/user-info.tsx
+++ resources/js/components/user-info.tsx
@@ -1,13 +1,16 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Badge } from '@/components/ui/badge';
 import { useInitials } from '@/hooks/use-initials';
 import type { User } from '@/types';
 
 export function UserInfo({
     user,
     showEmail = false,
+    showRole = false,
 }: {
     user: User;
     showEmail?: boolean;
+    showRole?: boolean;
 }) {
     const getInitials = useInitials();
 
@@ -21,6 +24,7 @@ export function UserInfo({
             </Avatar>
             <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">{user.name}</span>
+                {showRole && <Badge variant={'default'}>{user.role}</Badge>}
                 {showEmail && (
                     <span className="truncate text-xs text-muted-foreground">
                         {user.email}
